a1e8f3d41a55194b2251667a6e0ba699634112fb
towhid <towhidulhq@gmail.com>
2023-09-05 10:46:12 +0600

    Two sum added On2 version

--- twoSum.py
+++ twoSum.py
@@ -1,3 +1,7 @@
+
+
+# O(n)
+
 def towSums(arr , target):
     prev = {}
     
@@ -11,3 +15,15 @@ def towSums(arr , target):
 
 
 print(towSums([2,7,11,15], 9))        
+
+
+# O(n2)
+def towSums(arr , target):
+    n = len(arr)
+    for i in range(n):
+        for j in range(i + 1 , n):
+            if arr[i] + arr[j] == target:
+                return [i , j]
+print(towSums([2,7,11,15], 9)) 
+
+
