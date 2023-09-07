a1510cb04416cec25406cb9f378972b909af97af
towhid <towhidulhq@gmail.com>
2023-09-07 15:00:37 +0600

    Finding product 238

---
+++ problem-238.py
@@ -0,0 +1,32 @@
+import math
+def ProductsCount(nums):
+    b = []
+    for i in nums:
+        nums.remove(i)
+        b.append(math.prod(nums))
+        nums.insert(0, i)
+    return b 
+
+
+
+# [2,3,4,5]
+# [1,2,6,24] pre
+# [60,20,5,1]post
+# [60,40,30,24]
+def ProductsCount(nums):
+    l = len(nums)
+    p = [1] * l
+    
+    for i in range(1, l):
+        p[i] = p[i-1] * nums[i - 1]
+    r = nums[-1]
+    for i in range(l - 2, -1 , -1):
+        p[i] *= r
+        r *= nums[i]
+        
+    return p    
+
+print(ProductsCount([2,3,4,5]))
+            
+        
+    
