e1ab1d49176415e1ad1c9c7b7af92700c5e5d65b
towhid7667 <towhidulhq@gmail.com>
2024-02-14 00:01:49 +0600

    k element time limit exceeded

--- topK-347.py
+++ topK-347.py
@@ -50,6 +50,10 @@ def topKList(nums, k):
     count = {}
     freq = [[] for i in range(len(nums) + 1)]
 
+#     freq = []
+#   for _ in range(len(nums) + 1):
+#     freq.append([])
+
     for n in nums:
         count[n] = 1 + count.get(n, 0)
     for n, c in count.items():
