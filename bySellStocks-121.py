6a3afc796a4169e1738c8f192f2e778a7622ebd6
towhid7667 <towhidulhq@gmail.com>
2024-02-17 00:54:46 +0600

    buy and sell stock using sliding window

--- bySellStocks-121.py
+++ bySellStocks-121.py
@@ -33,15 +33,16 @@ def maxProfit(prices):
     #O(n)
 def maxProfit(prices):
         l , r = 0 , 1
-        max_prof = 0
+        max_profit = 0
+
         while r < len(prices):
-            if prices[r] > prices[l]:
-                max_prof = max(max_prof, prices[r] - prices[l])             
+            if prices[l] < prices[r]:
+                profit = prices[r] - prices[l]
+                max_profit = max(max_profit, profit)
             else:
                 l = r
-            r += 1 
-
-        return max_prof
+            r += 1
+        return max_profit  
     
     
     
